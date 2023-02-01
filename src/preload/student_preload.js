1f86c6852c4c1b29f2a6dd6a5710b066be6b452e
RoopjeetSingh <101702933+RoopjeetSingh@users.noreply.github.com>
2023-02-01 17:36:04 -0500

    Added comments

--- src/preload/student_preload.js
+++ src/preload/student_preload.js
@@ -1,37 +1,40 @@
+// Importing required modules
 const path = require('path');
 const fs = require('fs');
 
+// Function to read data from a JSON file
 function readFromJSON(file) {
-   return JSON.parse(fs.readFileSync(file));
+return JSON.parse(fs.readFileSync(file));
 }
 
+// Function to write data to a JSON file
 function writeToJSON(file, data) {
-   fs.writeFileSync(file, JSON.stringify(data, null, 2), {
-      encoding: 'utf-8',
-      flag: 'w'
-   });
-   console.log(`Wrote ${data} to ${file}`);
+fs.writeFileSync(file, JSON.stringify(data, null, 2), {
+encoding: 'utf-8',
+flag: 'w'
+});
+console.log(Wrote ${data} to ${file});
 }
 
+// Function to populate the select options
 function populateSelect() {
-   fetch(path.join(__dirname, '../database/events.json'))
-      .then(function (response) {
-         return response.json();
-      })
-      .then(function (events) {
-         let placeholder = document.querySelector('.event__options');
-         let output = '';
-         for (event of events) {
-            output += `
-               <option>${event.event_name}</option>
-            `
-         }
-         placeholder.innerHTML = output;
-      });
+fetch(path.join(__dirname, '../database/events.json'))
+.then(function (response) {
+return response.json();
+})
+.then(function (events) {
+let placeholder = document.querySelector('.event__options');
+let output = '';
+for (event of events) {
+output += <option>${event.event_name}</option> }
+placeholder.innerHTML = output;
+});
 }
 
+// Calling the populateSelect function
 populateSelect();
 
+// Function to show the form
 function showForm() {
-   document.querySelector('.form__title').innerHTML = document.querySelector('.event__options').value;
+document.querySelector('.form__title').innerHTML = document.querySelector('.event__options').value;
 }
