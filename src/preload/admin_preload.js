2f4738501dfab3fbf8225de5886e0a14f03bd7b1
Sai Nallani <sai.s.nallani@gmail.com>
2023-02-01 14:46:25 -0500

    Added alerts, added functionality to create student accounts

--- src/preload/admin_preload.js
+++ src/preload/admin_preload.js
@@ -1,6 +1,10 @@
-// Import the required libraries - path and fs
-const path = require("path");
-const fs = require("fs");
+const path = require('path');
+const fs = require('fs');
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
+let script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; // Check https://jquery.com/ for the current version
+document.getElementsByTagName('head')[0].appendChild(script);
 
 // Function to read from a JSON file
 function readFromJSON(file) {
