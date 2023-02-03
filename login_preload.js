171265b527e35da8be115461274fdc8bd545b509
Praneeth Rangarajan <121776157+RPraneeth05@users.noreply.github.com>
2023-02-02 21:57:29 -0500

    Update login_preload.js

--- login_preload.js
+++ login_preload.js
@@ -1,38 +1,65 @@
-// Importing required modules
-const path = require("path");
-const fs = require("fs");
+// // Importing required modules
+// const path = require("path");
+// const fs = require("fs");
 
-// Function to read data from a JSON file
+// // Function to read data from a JSON file
+// function readFromJSON(file) {
+//   return JSON.parse(fs.readFileSync(file));
+// }
+
+// // Function to write data to a JSON file
+// function writeToJSON(file, data) {
+//   fs.writeFileSync(file, JSON.stringify(data, null, 2), {
+//     encoding: "utf-8",
+//     flag: "w",
+//   });
+//   console.log(`Wrote ${data} to ${file}`);
+// }
+
+// // Function to validate the entered username and password
+// function validateUsernameAndPassword() {
+//   // Reading the list of users from the users.json file
+//   let users = readFromJSON(path.join(__dirname, "./src/database/users.json"));
+
+//   // Reading the entered username and password
+//   let usernameInput = document.querySelector(".username").value;
+//   let passwordInput = document.querySelector(".password").value;
+
+//   // Checking if the entered username and password match with any of the users
+//   for (user of users) {
+//     if (usernameInput === user.username && passwordInput === user.password) {
+//       // If the user is an admin, redirect to the admin page
+//       if (user.admin)
+//         location.href = path.join(__dirname, "./src/main/admin.html");
+//       // If the user is a student, redirecting to the student page
+//       else location.href = path.join(__dirname, "./src/main/student.html");
+//     }
+//   }
+// }
+
+const path = require('path');
+const fs = require('fs');
+
 function readFromJSON(file) {
-  return JSON.parse(fs.readFileSync(file));
+   return JSON.parse(fs.readFileSync(file));
 }
 
-// Function to write data to a JSON file
 function writeToJSON(file, data) {
-  fs.writeFileSync(file, JSON.stringify(data, null, 2), {
-    encoding: "utf-8",
-    flag: "w",
-  });
-  console.log(`Wrote ${data} to ${file}`);
+   fs.writeFileSync(file, JSON.stringify(data, null, 2), {
+      encoding: 'utf-8',
+      flag: 'w'
+   });
+   console.log(`Wrote ${data} to ${file}`);
 }
 
-// Function to validate the entered username and password
 function validateUsernameAndPassword() {
-  // Reading the list of users from the users.json file
-  let users = readFromJSON(path.join(__dirname, "./src/database/users.json"));
-
-  // Reading the entered username and password
-  let usernameInput = document.querySelector(".username").value;
-  let passwordInput = document.querySelector(".password").value;
-
-  // Checking if the entered username and password match with any of the users
-  for (user of users) {
-    if (usernameInput === user.username && passwordInput === user.password) {
-      // If the user is an admin, redirect to the admin page
-      if (user.admin)
-        location.href = path.join(__dirname, "./src/main/admin.html");
-      // If the user is a student, redirecting to the student page
-      else location.href = path.join(__dirname, "./src/main/student.html");
-    }
-  }
+   let users = readFromJSON(path.join(__dirname, './src/database/accounts.json'));
+   let usernameInput = document.querySelector('.username').value;
+   let passwordInput = document.querySelector('.password').value;
+   for (user of users) {
+      if (usernameInput === user.username && passwordInput === user.password) {
+         if (user.admin) location.href = path.join(__dirname, './src/main/admin.html');
+         else location.href = path.join(__dirname, './src/main/student.html');
+      }
+   }
 }
