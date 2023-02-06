58a3de09b1a787cd453a292a5c715e58a964181d
Sai Srautharsh Nallani Chakravartula <sai.s.nallani@gmail.com>
2023-02-06 01:03:40 -0500

    'hf'

--- src/database/tmp.py
+++ src/database/tmp.py
@@ -1,24 +1,21 @@
 import json
-import random
 
-# Load the events.json file
-with open("events.json", "r") as f:
-    events = json.load(f)
-
-# Load the users.json file
-with open("users.json", "r") as f:
-    users = json.load(f)
+# Load the users and events data from their respective files
+with open("users.json", "r") as users_file:
+    users = json.load(users_file)
+with open("events.json", "r") as events_file:
+    events = json.load(events_file)
 
 # Loop through each user
 for user in users:
-    if not user["admin"]:
-        # Choose a random number of events for the user to participate in
-        num_events = random.randint(1, len(events))
-        user_events = random.sample(events, num_events)
-        for event in user_events:
-            user["events"].append(event["event_name"])
-            user["points"] += int(event["prize"])
+    # Loop through each event
+    for event in events:
+        # Check if the user has the event in their events list
+        if not user['admin']:
+            if event["event_name"] in user["events"]:
+                # If they do, add their username to the participants list for that event
+                event["participants"].append(user["username"])
 
-# Save the updated users back to the users.json file
-with open("users.json", "w") as f:
-    json.dump(users, f)
+# Save the updated events data back to events.json
+with open("events.json", "w") as events_file:
+    json.dump(events, events_file)
